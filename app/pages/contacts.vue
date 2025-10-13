30403026014b063cb3d2405e75398b87b26ebe04
greyEyedOne <marina@rblab.net>
2025-10-13 16:06:20 +0200

    Add a title to a contacts page

--- app/pages/contacts.vue
+++ app/pages/contacts.vue
@@ -2,6 +2,7 @@
   <div
     class="contact-data flex flex-col gap-4 mt-6 text-gray-600 dark:text-white"
   >
+    <h1>Contacts:</h1>
     <span class="e-mail"
       ><span classs="text-strong">e-mail:</span>
       <a href="mailto:info@your-websmith.de">info@your-websmith.de</a></span
