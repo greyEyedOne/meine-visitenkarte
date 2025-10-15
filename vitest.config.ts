775d1566fb3418bedbe191c450cecd97d59e9ae8
Calvin Dudek <calvindudek@googlemail.com>
2025-10-15 10:13:13 +0200

    updated the scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue()],
   test: {
     globals: true,
     environment: 'happy-dom',
