13804220a5cbca2b7d3395f01d435f2c955e9bed
greyEyedOne <134321130+greyEyedOne@users.noreply.github.com>
2025-10-15 11:54:38 +0200

    Create maintenance.vue
    temporary page p.2

---
+++ pages/maintenance.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-gray-900 text-white text-center">
+<div>
+      <h1>We'll be back soon!</h1>
+
+  <p>
+    Meanwhile you can send me an <a href="mailto:appelmara@proton.me">e-mail</a> or connect me on
+    <a href="https://www.linkedin.com/in/marina-appel/">LinkedIn</a>
+  </p>
+      <p>Sincerely, Mari, your Websmith</p>
+    </div>
+  </div>
+</template>
