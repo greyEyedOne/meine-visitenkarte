8f54019e2bf58f81cde3d7d37bd2b9775d3c47fa
greyEyedOne <marina@rblab.net>
2025-10-17 17:46:59 +0200

    A pack of design improvements: - add a logo and a favicon - name rebrending - several adjustments

--- app/app.vue
+++ app/app.vue
@@ -2,8 +2,8 @@
   <main
     class="mx-auto pb-6 h-screen w-screen dark:bg-gray-900 bg-[url('/img/bg-template.png')] dark:bg-[url('/img/bg-dark-template.png')] bg-center bg-no-repeat bg-cover transition-all delay-150 duration-300 ease-in-out"
   >
-    <div class="backdrop-blur-sm h-full w-full">
-      <div class="flex flex-col items-center justify-center h-full mx-auto">
+    <div class="backdrop-blur-sm min-h-screen w-full">
+      <div class="flex flex-col items-center justify-center min-h-screen h-full mx-auto">
         <Header />
         <NuxtPage />
         <Footer />
--- app/components/Card.vue
+++ app/components/Card.vue
@@ -2,10 +2,8 @@
   <div
     class="card flex flex-col justify-center mt-auto mb-auto p-6 bg-white/70 dark:bg-slate-300/30 rounded-lg shadow-lg w-11/12 sm:w-3/4 md:w-1/2 lg:w-1/3"
   >
-    <h1
-      class="text-3xl font-bold text-black-500 uppercase dark:text-white text-center"
-    >
-      Mari Appel
+    <h1 class="text-3xl font-bold text-black-500 uppercase dark:text-white text-center">
+      Dara Appel
     </h1>
     <h2 class="text-center dark:text-white">Your Websmith</h2>
     <p class="slogan mt-4 text-gray-600 dark:text-white text-center">
