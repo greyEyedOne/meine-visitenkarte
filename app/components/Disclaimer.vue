77657c2c8cce30b819ca314b82ac424dd8677321
greyEyedOne <marina@rblab.net>
2025-12-16 16:28:14 +0100

    CTA added in the footer and the header, small fixes performed

--- app/components/Disclaimer.vue
+++ app/components/Disclaimer.vue
@@ -3,8 +3,8 @@
     class="disclaimer box-border text-center dark:text-amber-500 text-amber-600 w-full mt-6 mb-4"
   >
     Dear visitors, <br />
-    the site is currently under development.<br />
-    <span>
+    the page is currently under development.<br />
+    <span class="underline font-bold">
       <NuxtLink to="/contacts">Let's stay in touch</NuxtLink>
     </span>
   </div>
