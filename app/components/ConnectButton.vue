c149d9349f10894b4cedd46319dc949d8f79ff4d
greyEyedOne <marina@rblab.net>
2026-01-03 21:12:45 +0100

    button opacity changed

--- app/components/ConnectButton.vue
+++ app/components/ConnectButton.vue
@@ -2,7 +2,7 @@
   <button class="connect mt-6 text-md uppercase rounded-md w-fit mx-auto">
     <NuxtLink
       to="/contacts"
-      class="px-4 py-2 block box-border cursor-pointer rounded-md bg-black dark:bg-neutral-100 text-white dark:text-black hover:bg-indigo-900 dark:hover:text-indigo-100"
+      class="px-4 py-2 block box-border cursor-pointer rounded-md bg-black dark:bg-neutral-100 text-white dark:text-black hover:bg-indigo-900 dark:hover:text-indigo-100 opacity-80 dark:opacity-90 hover:opacity-100"
     >Connect with me</NuxtLink
     >
   </button>
