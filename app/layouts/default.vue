6cf18422500b885479f43d466dd404eb03d4f07b
greyEyedOne <marina@rblab.net>
2026-01-07 15:10:16 +0100

    add a default layout file

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// const locale = useCookie('locale', { default: () => 'en' })
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col min-h-screen h-full mx-auto justify-center items-center">
+      <Header />
+      <div class="container flex w-full max-w-95/100 h-full mx-6">
+        <NuxtPage />
+      </div>
+      <Footer />
+    </div>
+  </div>
+</template>
