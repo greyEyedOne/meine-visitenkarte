e78ecb686ac77538e0fadebd192fc2b5aedc5979
greyEyedOne <marina@rblab.net>
2026-02-25 20:32:27 +0100

    Create a page for a portfolio case

---
+++ app/pages/portfolio-cases/responsive-product-catalogue.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Catalogue from '~/components/portfolio/Catalogue.vue'
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/portfolio" class="navigation mr-auto before:content-['<<'] before:mr-1">
+      Back to portfolio</NuxtLink
+    >
+    <div class="case-preview-card">
+      <Catalogue />
+    </div>
+  </div>
+</template>
